fa1f576c7dd794ae8134719793d10ed4c84d0b80
Jakub Zymelka <jakub.zymelka@nordicsemi.no>
2023-10-09 17:55:13 +0200

    samples: shell: fix for POF default values
    JIRA: NRFX-4623
    
    Signed-off-by: Jakub Zymelka <jakub.zymelka@nordicsemi.no>

--- samples/shell/src/main.c
+++ samples/shell/src/main.c
@@ -26,7 +26,7 @@ void main(void)
 	const struct device *pmic_dev = DEVICE_DT_GET(DT_NODELABEL(npm_0));
 
 	if (!device_is_ready(pmic_dev)) {
-		LOG_INF("PMIC device is not ready.");
+		LOG_ERR("PMIC device is not ready.");
 		return;
 	}
 
@@ -37,11 +37,16 @@ void main(void)
 	npmx_ldsw_active_discharge_enable_set(npmx_ldsw_get(npmx_instance, 0), true);
 	npmx_ldsw_active_discharge_enable_set(npmx_ldsw_get(npmx_instance, 1), true);
 
-	/* Register callback so that POF warnings can be printed. */
-	npmx_pof_config_t pof_config = { .status = NPMX_POF_STATUS_ENABLE,
-					 .threshold = NPMX_POF_THRESHOLD_3V5,
-					 .polarity = NPMX_POF_POLARITY_HIGH };
+	npmx_pof_t *pof_instance = npmx_pof_get(npmx_instance, 0);
 
+	npmx_pof_config_t pof_config;
+	npmx_error_t err_code = npmx_pof_config_get(pof_instance, &pof_config);
+	if (err_code != NPMX_SUCCESS) {
+		LOG_ERR("Unable to read POF config.");
+		return;
+	}
+
+	/* Register callback so that POF warnings can be printed. */
 	npmx_driver_register_pof_cb(pmic_dev, &pof_config, pof_callback);
 
 	while (1) {
